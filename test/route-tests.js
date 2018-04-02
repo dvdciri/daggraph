645c188fe2616c716ea6620c890b52a9e3d5dfc3
Cirillo, Davide (Android Developer) <davide.cirillo@sky.uk>
2018-04-02 17:35:21 +0100

    Init router tests

---
+++ test/route-tests.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+import assert from 'assert';
+import run, { UP, DOWN, ENTER } from 'inquirer-test';
+const path = require('path');
+const ROUTER = require('./../src/router');
+
+/* start ################################### of java_dagger_defaut tests */
+
+test("Given custom path", t => {
+    
+    // ROUTER.init([path.join(__dirname, "data/java_dagger_default")], undefined)
+    t.fail("Write some tests please")
+});
